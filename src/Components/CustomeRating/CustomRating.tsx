472269958675f7c0afe626e8f48bab3207241a32
Evgenii <evgusa83@gmail.com>
2023-10-09 16:14:11 +0300

    Custome Rating, animation stars

--- src/Components/CustomeRating/CustomRating.tsx
+++ src/Components/CustomeRating/CustomRating.tsx
@@ -1,22 +1,56 @@
 import React, {useCallback, useEffect, useMemo, useState} from "react";
 import {Stars} from "Components/CustomeRating/Stars";
+import style from '../CustomeRating/StarsStyle.module.css'
 //import { IconName } from "react-icons/ci";
 // CiFaceFrown CiFaceMeh CiFaceSmile
 
+
+const message = 'oops, maybe there are more stars than statuses. Please add status for your variant'
+export type CommentRatingType = 'Useless' | 'Poor' | 'Ok' | 'Good' | 'Excellent' | typeof message | ''
+
 export const CustomRating: React.FC = () => {
     const [rating, setRating] = useState<number | null>(null)
+    const [commentRating, setCommentRating] = useState<CommentRatingType>('')
     const maxValue = 5;
+
+
+
     const onChangeRating = useCallback( (count: number) => {
         setRating(count)
+        switch (count){
+            case 1 : {
+                setCommentRating("Useless")
+                break
+            }
+            case 2: {
+                setCommentRating('Poor')
+                break
+            }
+            case 3: {
+                setCommentRating("Ok")
+                break
+            }
+            case 4: {
+                setCommentRating("Good")
+                break
+            }
+            case 5: {
+                setCommentRating("Excellent")
+                break
+            }
+            default: setCommentRating(message)
+        }
     }, [rating])
 
-
-    const memoizedStars = useMemo(() =>
-        <Stars maxValue={maxValue} onChangeRating={onChangeRating} value={rating} />,
-        [maxValue, onChangeRating, rating]);
         return (
             <div>
-                {memoizedStars}
+                <Stars maxValue={maxValue}
+                       onChangeRating={onChangeRating}
+                       value={rating}
+                       showGrade = {true}
+                       comment = {commentRating}
+
+                />
             </div>
         )
 }
--- src/Components/CustomeRating/Stars.tsx
+++ src/Components/CustomeRating/Stars.tsx
@@ -1,32 +1,69 @@
-import React, {useCallback, useState} from "react";
-import style from '../CustomeRating/StarsStyle.module.css'
-import {IoMdStar} from "react-icons/io";
-//LiaStarHalf
-//IoStarHalf
+import React, { useCallback, useEffect, useState } from "react";
+import style from '../CustomeRating/StarsStyle.module.css';
+import { IoMdStar } from "react-icons/io";
+import { CommentRatingType } from "Components/CustomeRating/CustomRating";
 
 type CustomRatingType = {
-    maxValue: number
-    onChangeRating: (count: number)=> void
-    value: number | null
-}
+    maxValue: number;
+    onChangeRating: (count: number) => void;
+    value: number | null;
+    showGrade: boolean;
+    comment: CommentRatingType;
+};
 
 export const Stars: React.FC<CustomRatingType> = ({
                                                       maxValue,
                                                       onChangeRating,
-                                                      value
+                                                      value,
+                                                      showGrade,
+                                                      comment
                                                   }) => {
+    const [activeStar, setActiveStar] = useState<number | null>(null);
+
+    /// functions for choose stars
+    const onFocusForStars = useCallback((currentStar: number) => {
+        setActiveStar(currentStar);
+    }, [activeStar]);
+
+    useEffect(() => {
+        // Функция для применения анимации к звездам по их id
+        function applyAnimationById(id: string) {
+            const star = document.getElementById(id);
+            if (star) {
+                star.classList.add(style.bounce);
+                setTimeout(() => {
+                    star.classList.remove(style.bounce);
+                }, 1000);
+            }
+        }
+        // Если value изменилось, применяем анимацию
+        if (value !== null) {
+            for (let i = 1; i <= value; i++) {
+                applyAnimationById(`star-${i}`);
+            }
+        }
+    }, [value]);
 
     return (
-        <div>
-            {Array.from({length: maxValue}).map((_,i) => {
-                let currentStar = i + 1
+        <div className={style.starContainer}>
+            {Array.from({ length: maxValue }).map((_, i) => {
+                let currentStar = i + 1;
                 return (
                     <span
+                        id={`star-${currentStar}`}
                         key={currentStar}
-                          className={value && currentStar <= value  ? style.isColorStar: style.testNon}
-                          onClick={()=> onChangeRating(currentStar)}>{<IoMdStar className={style.stars}/>}</span>
-                )})
-            }
+                        className={activeStar && currentStar <= activeStar ? style.isColorStar : style.isNotColorStar}
+                        onMouseOver={() => onFocusForStars(currentStar)}
+                        onMouseLeave={() => setActiveStar(value)}
+                        onClick={() => {
+                            onChangeRating(currentStar);
+                        }}
+                    >
+            {<IoMdStar className={style.stars} />}
+          </span>
+                );
+            })}
+            {showGrade && <span className={style.grade}>{comment}</span>}
         </div>
-    )
-}
+    );
+};
